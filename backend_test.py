9a05b63019bddb342ff056a788ec25f401f01011
ankeshostwal <ankeshostwal@github.com>
2026-01-16 22:13:05 +0000

    auto-commit for 88356598-1db8-45ae-b957-3d1f8074933b

--- backend_test.py
+++ backend_test.py
@@ -425,7 +425,7 @@ class VehicleFinanceAPITester:
 
 def main():
     # Backend URL from the review request
-    backend_url = "https://anjaar-finance.preview.emergentagent.com"
+    backend_url = "https://contract-view-1.preview.emergentagent.com"
     
     tester = VehicleFinanceAPITester(backend_url)
     summary = tester.run_all_tests()
